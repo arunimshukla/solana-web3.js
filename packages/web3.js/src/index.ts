b7b984a19baf8df14a8f852cfa16aea373ecb176
Arunim Shukla <54760103+arunimshukla@users.noreply.github.com>
2026-09-21 15:26:03 +0530

    feat(web3.js): re-export @solana/codecs

--- packages/web3.js/src/index.ts
+++ packages/web3.js/src/index.ts
@@ -1,4 +1,6 @@
 export {type Blockhash} from '@solana/kit';
+// eslint-disable-next-line import/export -- @solana/codecs is itself a star-export barrel.
+export * from '@solana/codecs';
 export * from './bpf-loader-deprecated';
 export * from './bpf-loader';
 export * from './connection';
